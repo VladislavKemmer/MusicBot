bda33544b3fa6f23c55fd707d1ad2bcf9f3767f9
VladislavKemmer <168862338+VladislavKemmer@users.noreply.github.com>
2024-05-04 12:58:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 /*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
+               
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## Created by Kemmer!                                                 ║
+║  ## Feel free to utilize any portion of the code                       ║   
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
@@ -164,20 +156,12 @@ module.exports = client;
 
 
 /*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
+               
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## Created by Kemmer!                                                 ║
+║  ## Feel free to utilize any portion of the code                       ║   
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
